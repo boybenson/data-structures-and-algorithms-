10842549571cb867b2841d62f9f996041b4ee8d5
boybenson <ybenson96@gmail.com>
2023-07-06 10:21:55 +0000

    feat: about to learn quick sort

--- index.js
+++ index.js
@@ -1,15 +1,5 @@
-const insertionSort = (arr) => {
-  for (let i = 1; i < arr.length; i++) {
-    let numberToInsert = arr[i];
-    let j = i - 1;
-    while (j >= 0 && arr[j] > numberToInsert) {
-      arr[j + 1] = arr[j];
-      j = j - 1;
-    }
-    arr[j + 1] = numberToInsert;
-  }
-};
+const quickSort = (arr) => {};
 
 const arr = [10, 3, 8, 20, 15, 76];
-insertionSort(arr);
+quickSort(arr);
 console.log(arr);
