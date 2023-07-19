c32976fe50f1e550928b6e6e5782b33b8532cac1
boybenson <ybenson96@gmail.com>
2023-07-19 08:18:42 +0000

    feat: changes

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const quickSort = (arr) => {};
+const mergeSort = (arr) => {
+  if (arr.length <= 1) {
+    return arr;
+  }
+  const mid = Math.floor(arr.length / 2);
+  const leftArr = mergeSort(arr.slice(0, mid));
+  const rightArr = mergeSort(arr.slice(mid));
 
-const arr = [10, 3, 8, 20, 15, 76];
-quickSort(arr);
-console.log(arr);
+  return merge(leftArr, rightArr);
+};
+
+function merge(leftArr, rightArr) {
+  let sorted = [];
+
+  while (leftArr.length && rightArr.length) {
+    if (leftArr[0] < rightArr[0]) {
+      sorted.push(leftArr.shift());
+    } else sorted.push(rightArr.shift());
+  }
+
+  return [...sorted, ...leftArr, ...rightArr];
+}
+
+let arr = [1, 6, 2, 3, 9, -1, 7];
+const finalSortedArray = mergeSort(arr);
+console.log(finalSortedArray);
---
+++ sorting_algorithms/merge_sort.js
@@ -0,0 +1,3 @@
+const mergeSort = () => {};
+
+console.log(mergeSort());
